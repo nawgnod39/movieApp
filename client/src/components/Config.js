ff69ff4bfb2ac5940201e29389c9529b0070c375
takdongwan <dongwantak@gmail.com>
2021-01-10 22:20:23 +0900

    영화정보,영화이미지 Landing Page 생성

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,4 +1,5 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
 export const API_URL = 'https://api.themoviedb.org/3/';
-export const IMAGE_BASE_URL = 'http://imahe.tmdb.org/t/p/';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+export const API_KEY= 'dd125d5326fc6ce4b694b02b2253c7c3'
--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
-
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
