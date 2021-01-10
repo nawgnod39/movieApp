d8d71c5bc7095113333ee38f4e0e975017137140
takdongwan <dongwantak@gmail.com>
2021-01-10 14:48:53 +0900

    boiler-plate & MongoDB연결 & movie api 설정

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+
+function LandingPage() {
+    return (
+        <div style = {{width:'100%',margin: '0'}}>
+        
+            {/*main image*/}
+            <div style ={{width'85%',margin:'1rem auto'}}>
+
+                <h2>Movies by latest</h2>
+            </div>
+        </div>}
+        </>
+    )
+}
+
+export default LandingPage
