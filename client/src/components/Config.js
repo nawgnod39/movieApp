c54b89e28928bf07bab20bceab9eda803227c811
takdongwan <dongwantak@gmail.com>
2021-01-15 17:00:24 +0900

    Like,Dislike 생성

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,3 +3,10 @@ export const USER_SERVER = '/api/users';
 export const API_URL = 'https://api.themoviedb.org/3/';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 export const API_KEY= 'dd125d5326fc6ce4b694b02b2253c7c3'
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500' 
